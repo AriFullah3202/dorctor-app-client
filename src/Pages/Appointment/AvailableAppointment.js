4866777b3741b01c3142ba4cd4f2aa695b5e8f0d
Arif <marifullaah@gmail.com>
2022-11-16 13:07:49 +0600

    fetch data using react query

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -2,20 +2,32 @@ import React, { useState, useEffect } from 'react'
 import { format } from 'date-fns/esm';
 import AppointmentOption from './AppointmentOption';
 import BookingModel from './BookingModel';
+import { useQuery } from '@tanstack/react-query';
 
 const AvailableAppointment = ({ selectedDate }) => {
-    const [appointmentOptions, setAppointmentOptions] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/appointmentOptions`)
-            .then(res => res.json())
-            .then(data => {
-                setAppointmentOptions(data)
-                console.log(data)
-            })
-            .catch(err => console.log(err))
-    }, [])
+
+    const { data: appointmentOptions = [] } = useQuery({  //useQuery 2ta parameter nibe 
+
+        queryKey: ['appointmentOptions'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/appointmentOptions`)
+            const data = await res.json();
+            console.log(data)
+            return data;
+        }
+    })
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/appointmentOptions`)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             setAppointmentOptions(data)
+    //             console.log(data)
+    //         })
+    //         .catch(err => console.log(err))
+    // }, [])
 
     return (
         <section className="mt-15">
--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import 'react-day-picker/dist/style.css'
 import AuthProvider from './context/AuthProvider';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
 
   </React.StrictMode>
 );
