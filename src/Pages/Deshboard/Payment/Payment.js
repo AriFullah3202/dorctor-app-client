03047415741fb3a384b14361aacdfadaf24451b4
Arif <marifullaah@gmail.com>
2022-11-22 01:48:08 +0600

    added react router hook for loading

--- src/Pages/Deshboard/Payment/Payment.js
+++ src/Pages/Deshboard/Payment/Payment.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigation } from 'react-router';
 import CheckoutForm from './CheckoutForm';
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements } from '@stripe/react-stripe-js'
+import Loading from '../../Shared/Loading';
 
 
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK);
@@ -10,7 +11,11 @@ console.log(stripePromise)
 
 const Payment = () => {
     const booking = useLoaderData();
+    const navigation = useNavigation();
     const { treatment, price, appointmentDate, slot } = booking;
+    if (navigation.state === 'loading') {
+        <Loading></Loading>
+    }
     return (
         <div>
             <h1 className="text-3xl">Payment for {treatment}</h1>
